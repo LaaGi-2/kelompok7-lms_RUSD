379f2bfb31c74e2ab8b198724eb3460604dd89fa
fshoffii <s.shoffii@gmail.com>
2026-04-20 10:07:39 +0700

    form janji temu
    menambahkan folder janji temu dan index phpp agar bisa menginputkan data

---
+++ view/janji temu/tambah.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p> Nama Pasien: <?php echo $_POST["nama_pasien"]; ?></p>
+    <p> No WhatsApp: <?php echo $_POST["no_whatsapp"]; ?></
+    <p> Layanan: <?php echo $_POST["layanan"]; ?></p>
+    <p> Tanggal Kunjungan: <?php echo $_POST["tanggal_kunjungan"]; ?></p>
+</body>
